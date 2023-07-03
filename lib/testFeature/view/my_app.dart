e5a88fa7deaceb659d73d7f614284ff5a7ba4a5d
valentinallavayol <vllavayol@gmail.com>
2023-07-03 00:31:11 -0300

    Feat: Updated message text to show a list of messages

--- lib/testFeature/view/my_app.dart
+++ lib/testFeature/view/my_app.dart
@@ -24,22 +24,60 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(20),
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 6),
         child: BlocBuilder<ChatCubit, ChatState>(
           builder: (context, state) {
             print("PRINTEO ESTADO ${state.messages}");
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            return Stack(
+              // crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Form(
-                  child: TextFormField(
-                    controller: _controller,
-                    decoration:
-                        const InputDecoration(labelText: 'Send a message'),
+                Positioned(
+                  top: 0,
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: ListView(
+                    children:
+                        state.messages.map((message) => Text(message)).toList(),
+                  ),
+                )
+                // const SizedBox(height: 24),
+                ,
+                Positioned(
+                  bottom: 0,
+                  child: SizedBox(
+                    width: MediaQuery.sizeOf(context).width,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Expanded(
+                          child: Form(
+                            child: TextFormField(
+                              controller: _controller,
+                              decoration: const InputDecoration(
+                                  labelText: 'Send a message'),
+                            ),
+                          ),
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.send),
+                          onPressed: () => context
+                              .read<ChatCubit>()
+                              .sendMessage(_controller.text),
+                          color: Colors.black,
+                          iconSize: 30,
+                          // child: Text("Send")),
+                        ),
+                        // ElevatedButton(
+                        //     onPressed: () => context
+                        //         .read<ChatCubit>()
+                        //         .sendMessage(_controller.text),
+                        //     child: Text('asdsd'))
+                      ],
+                    ),
                   ),
                 ),
-                const SizedBox(height: 24),
-                ...state.messages.map((message) => Text(message)),
                 // BlocBuilder<MessageCubit, MessageState>(
                 //   builder: (context, state) {
                 //     return Text(state);
@@ -50,12 +88,6 @@ class _MyHomePageState extends State<MyHomePage> {
           },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () =>
-            context.read<ChatCubit>().sendMessage(_controller.text),
-        tooltip: 'Send message',
-        child: const Icon(Icons.send),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
